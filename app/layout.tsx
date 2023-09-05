5a2e26996a459f72155e99061cdb26d365d89a81
HashirHussain <hashir.hussain111@gmail.com>
2023-09-05 14:20:35 +0530

    prettier fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Wordle game',
-  description: 'Wordle game clone in nextjs by hashir hussain',
-}
+  title: "Wordle game",
+  description: "Wordle game clone in nextjs by hashir hussain",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
