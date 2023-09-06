bf8ce81cc4531e77b85d5c4d83157cc302bc0d45
HashirHussain <hashir.hussain111@gmail.com>
2023-09-06 19:06:05 +0530

    Remove alert backdrop

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -1,9 +1,8 @@
-const alertStyle = `bg-gray-50 border-slate-100 border-2 text-gray-800 drop-shadow-2xl px-8 py-6 rounded absolute`;
+const alertStyle = `absolute rounded px-8 py-6 bg-gray-50 border-slate-100 border-2 text-gray-800 drop-shadow-2xl`;
 
-export default function Alert(props: { children: any; }) {
+export default function Alert(props: { children: any }) {
     return (
         <>
-            <div className="absolute h-screen w-full bg-slate-50 opacity-50 drop-shadow-2xl"></div>
             <div className={`${alertStyle}`} role="alert">
                 <span className="block sm:inline font-semibold">{props.children}</span>
             </div>
