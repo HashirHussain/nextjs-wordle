c05773c88bca9ecd64c23cf75e0f0fe4a602b974
HashirHussain <hashir.hussain111@gmail.com>
2023-09-07 13:12:38 +0530

    fix dark theme

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data: { wordsList: Array<string> } = await getData();
 
   return (
-    <section className="flex flex-col justify-center m-auto relative">
+    <section className="flex flex-col justify-center m-auto relative bg-inherit dar:bg-inherit">
       <Game wordsList={data.wordsList} />
     </section>
   );
