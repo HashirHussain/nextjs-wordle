78d16156b1b842c6a6a4276eb156ef266c2f78e9
HashirHussain <hashir.hussain111@gmail.com>
2023-09-07 13:09:49 +0530

    fix dark theme

--- app/components/PlayBoard.tsx
+++ app/components/PlayBoard.tsx
@@ -6,9 +6,9 @@ import {
 } from "../lib";
 
 const blockStyle = `h-12 w-12 sm:w-14 sm:h-14 grid place-items-center p-0 m-0 font-bold text-2xl border-2 rounded-md border-gray-200 text-gray-600 dark:text-gray-50 uppercase`;
-const rightPositionStyle = `animate-[ping_75ms] bg-lime-600 border-lime-600 text-gray-50`;
-const wrongPositionStyle = `bg-amber-400 border-amber-400 text-gray-50`;
-const noPositionStyle = `bg-gray-500 border-gray-500 text-gray-50`;
+const rightPositionStyle = `animate-[ping_75ms] bg-lime-600 border-lime-600 dark:bg-lime-600 dark:border-lime-600 text-gray-50`;
+const wrongPositionStyle = `bg-amber-400 border-amber-400 dark:bg-amber-400 dark:border-amber-400 text-gray-50`;
+const noPositionStyle = `bg-gray-500 border-gray-500 dark:bg-gray-500 dark:border-gray-500 text-gray-50`;
 
 type PropsType = {
   chanceLimit: number;
